cd1743ed088a03e55d38f1f46f67787d3dc99552
Gaurav Kushwaha <kushwahagaurav062@gmail.com>
2025-08-17 13:57:28 +0530

    Update CORS for frontend deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,7 @@ const app = express();
 // ===== Middleware =====
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  "http://localhost:5173", // during local dev
+const allowedOrigins = [ 
   "https://mybooklibraryy.netlify.app/", // your deployed frontend
 ];
 app.use(cors({
